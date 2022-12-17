7f7f24ea267cc916666838eb5391406b92c47dd0
hayas1 <h4ystack@gmail.com>
2022-12-17 05:07:14 +0000

    comment out dyson

--- terraform/ghrepo_dyson.tf
+++ terraform/ghrepo_dyson.tf
@@ -1,23 +1,28 @@
-resource "github_repository" "dyson" {
-  name        = "dyson"
-  description = "dynamic json parser implemented by rust"
-  visibility  = "public"
+# resource "github_repository" "dyson" {
+#   name        = "dyson"
+#   description = "dynamic json parser implemented by rust"
+#   visibility  = "public"
 
-  has_downloads = true
-  has_issues    = true
-  has_projects  = true
-  has_wiki      = true
+#   has_downloads = true
+#   has_issues    = true
+#   has_projects  = true
+#   has_wiki      = true
+#   pages {
+#     source {
+#       branch = "GitHub Actions"
+#     }
+#   }
 
-  security_and_analysis {
-    advanced_security {
-      status = "enabled"
-    }
-    secret_scanning {
-      status = "enabled"
-    }
-    secret_scanning_push_protection {
-      status = "enabled"
-    }
-  }
-}
+#   security_and_analysis {
+#     advanced_security {
+#       status = "enabled"
+#     }
+#     secret_scanning {
+#       status = "enabled"
+#     }
+#     secret_scanning_push_protection {
+#       status = "enabled"
+#     }
+#   }
+# }
 
