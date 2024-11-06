de626741461e817504b28267f700f9f716edc452
hayas1 <h4ystack@gmail.com>
2024-11-06 14:58:18 +0000

    make repository data source

--- terraform/ghrepo_this.tf
+++ terraform/ghrepo_this.tf
@@ -1,18 +1,12 @@
-resource "github_repository" "tfgh" {
-  name        = "tfgh"
-  description = "managed by terraform"
-  visibility  = "public"
-
-  lifecycle {
-    prevent_destroy = true
-  }
+data "github_repository" "tfgh" {
+  full_name = "hayas1/tfgh"
 }
 
 resource "github_repository_environment" "plan" {
   environment = "plan"
-  repository  = github_repository.tfgh.name
+  repository  = data.github_repository.tfgh.name
 }
 resource "github_repository_environment" "apply" {
   environment = "apply"
-  repository  = github_repository.tfgh.name
+  repository  = data.github_repository.tfgh.name
 }
