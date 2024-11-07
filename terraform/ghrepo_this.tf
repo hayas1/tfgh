839fb71d72954c2360803ebf57ea4acb70cbf5df
hayas1 <h4ystack@gmail.com>
2024-11-07 15:40:44 +0000

    protect main branch

--- terraform/ghrepo_this.tf
+++ terraform/ghrepo_this.tf
@@ -2,6 +2,28 @@ data "github_repository" "tfgh" {
   full_name = "hayas1/tfgh"
 }
 
+resource "github_repository_ruleset" "tfgh" {
+  name        = "default"
+  repository  = data.github_repository.tfgh.name
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["~DEFAULT_BRANCH"]
+      exclude = []
+    }
+  }
+
+  rules {
+    creation = true
+    update   = true
+    deletion = true
+
+    pull_request {}
+  }
+}
+
 resource "github_repository_environment" "plan" {
   environment = "plan"
   repository  = data.github_repository.tfgh.name
@@ -9,4 +31,9 @@ resource "github_repository_environment" "plan" {
 resource "github_repository_environment" "apply" {
   environment = "apply"
   repository  = data.github_repository.tfgh.name
+
+  deployment_branch_policy {
+    protected_branches     = true
+    custom_branch_policies = false
+  }
 }
