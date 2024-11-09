60bd94b41d1541265d20d34e232f5b9ea23ac999
hayas1 <h4ystack@gmail.com>
2024-11-09 06:14:27 +0000

    labeler.yml / release.yml

--- terraform/module/repository/files.tf
+++ terraform/module/repository/files.tf
@@ -3,23 +3,34 @@ locals {
   managed_pr_branch                = "chore/managed-by-terraform"
   managed_pr_commit_message_prefix = "Managed by Terraform"
   managed_pr_title                 = "files managed by Terraform"
+  managed_pr_body = join("\n", [
+    for f in local.github_files : "- .${f}"
+  ])
 
-  managed_pr_body = <<EOT
-  - pull request template
-  EOT
+  github_files = fileset(path.module, "github/**")
 }
 
-resource "github_repository_file" "pull_request_template" {
-  for_each            = github_repository.this
-  repository          = each.value.name
+resource "github_repository_file" "this" {
+  for_each = {
+    for p in setproduct(
+      [for k, v in github_repository.this : { key = k, value = v }],
+      local.github_files
+    )
+    : "${p[0].key}.${p[1]}" => {
+      repository = p[0].value
+      local_path = p[1]
+    }
+  }
+
+  repository          = each.value.repository.name
   branch              = local.managed_pr_branch
-  file                = ".github/pull_request_template.md"
-  content             = file("${path.module}/github/pull_request_template.md")
-  commit_message      = "${local.managed_pr_commit_message_prefix}: pull request template"
+  file                = ".${each.value.local_path}"
+  content             = file("${path.module}/${each.value.local_path}")
+  commit_message      = "${local.managed_pr_commit_message_prefix}: .${each.value.local_path}"
   overwrite_on_create = true
 
   autocreate_branch               = true
-  autocreate_branch_source_branch = each.value.default_branch
+  autocreate_branch_source_branch = each.value.repository.default_branch
 }
 
 resource "github_repository_pull_request" "managed" {
@@ -30,5 +41,5 @@ resource "github_repository_pull_request" "managed" {
   title           = local.managed_pr_title
   body            = local.managed_pr_body
 
-  depends_on = [github_repository_file.pull_request_template]
+  depends_on = [github_repository_file.this]
 }
